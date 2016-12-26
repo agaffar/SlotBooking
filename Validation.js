465b23e4e28e80717b5fd7fa9afbd2b2d8e2b286
agaffar <abdul.gaffar@india.semanticbits.com>
2016-12-26 20:56:12 +0530

    new with slotbooking and json formatted for the data respect to the user

--- Validation.js
+++ Validation.js
@@ -1,25 +1,80 @@
 /**
  * Created by Lenovo on 12/23/2016.
  */
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'SampleData.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+// Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
 function validation(){
     var val1 = document.myform.fname;
     var val2 = document.myform.pwd;
-    if(val1.value == "" )
-    {
-        //alert("hello");
-        document.getElementById('error').innerHTML = '<font color="red">enter the email id</p>';
-    }
-    if(val2.value == "" )
-    {
-        document.getElementById("error").innerHTML = "<font color='red'>invalid username or  password</p>";
-    }
-    else
-    {
-        //alert("password   "+val2);
-        //document.getElementById("error").innerHTML = "<font color='green'>your elgible for the operation</p>";
-        localStorage.setItem("userEmailId",val1.value);
-        window.location.href = "DashBoard.html";
-    }
+
+
+        loadJSON(function (response) {
+// Parse JSON string into object
+            var jsonObject = JSON.parse(response);;
+            if(localStorage.hasOwnProperty("jsonObject"))
+            {
+                console.log("has");
+                jsonObject = JSON.parse(localStorage.getItem("jsonObject"));
+            }
+            else {
+                jsonObject = JSON.parse(response);
+                jsonObject =localStorage.setItem("jsonObject",JSON.stringify(jsonObject));
+
+            }
+            if(val1.value == "" )
+            {
+                //alert("hello");
+                document.getElementById('error').innerHTML = '<font color="red">enter the email id</p>';
+            }
+            if(val2.value == "" )
+            {
+                document.getElementById("error").innerHTML = "<font color='red'>invalid username or  password</p>";
+            }
+            else
+            {
+                //alert("password   "+val2);
+                //document.getElementById("error").innerHTML = "<font color='green'>your elgible for the operation</p>";
+
+                for(var i=0;i<jsonObject.user.length;i++)
+                {
+                    var user = jsonObject.user[i];
+                    //alert("jsonObject.user.length=="+jsonObject.user.length);
+
+                    console.log(user.email_id);
+
+                    if(user.email_id == val1.value)
+                    {
+                        localStorage.setItem("userEmailId",val1.value);
+                        if(user.role == "employee")
+                        {
+                            console.log("employee");
+                            alert("employeee");
+                            window.location.href = "EmployeeDashBoard.html";
+                        }
+                        else
+                        {
+                            window.location.href = "DashBoard.html";
+                        }
+
+                    }
+
+                }
+
+            }
+        });
+
+
     return false;
 }
 function validate()
